e4135c6d11ecf6d1f26616cc369a8958eaf1f56d
Rajeev <rajeev@localhost.localdomain>
2021-03-06 13:32:05 +0100

    Controllers are working now

--- public/index.php
+++ public/index.php
@@ -7,17 +7,22 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 // imports
 use app\core\Application;
+use app\controllers\ContactController;
+use app\controllers\AuthController;
 
 // create application
 $app = new Application(dirname(__DIR__));
 
 // define routes
-$app->router->get('/', function() {
-    return 'Hello man bro';
-});
+$app->router->get('/', [ContactController::class, 'home']);
 
-$app->router->get('/contact', 'contact');
-$app->router->get('/', 'home');
+$app->router->get('/contact', [ContactController::class, 'contact']);
+$app->router->post('/contact', [ContactController::class, 'handleContact']);
+
+$app->router->get('/login', [AuthController::class, 'login']);
+$app->router->get('/register', [AuthController::class, 'register']);
+$app->router->post('/login', [AuthController::class, 'login']);
+$app->router->post('/register', [AuthController::class, 'register']);
 
 // execute app
 $app->run();
